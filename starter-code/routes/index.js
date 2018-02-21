cb6a4002262ac7db700d463f89075c02a0ae9316
Ravi Kalia <ravkalia@gmail.com>
2018-02-21 13:54:52 -0600

    iteration 1 and 2

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -3,7 +3,19 @@ const router  = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+  res.render('index');
+});
+
+router.post('/create', (req, res, next) => {
+  res.render('create');
+});
+
+router.get('/show', (req, res, next) => {
+  res.render('show');
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('new');
 });
 
 module.exports = router;
